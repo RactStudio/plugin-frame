1c9557e3039fe93280d82de23d61fbaedbe14a1a
mhrubel <bd.mhrubel@gmail.com>
2024-11-16 21:22:48 +0600

    updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
                 src: [
                     '**',
                     '!node_modules/**',
+                    '!storage/**',
                     '!.pluginframe',
                     '!composer.json',
                     '!composer.lock',
