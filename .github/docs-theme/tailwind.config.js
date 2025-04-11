b31b7ca14e03bca38d2b2423d29628d770ef93a8
mhrubel <bd.mhrubel@gmail.com>
2025-04-11 22:31:21 +0600

    update

---
+++ .github/docs-theme/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      '../public/**/*.html',
+      '../templates/**/*.twig'
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'wp-blue': '#21759b',
+          'wp-gray': '#f1f1f1'
+        }
+      }
+    }
+  }
