6358864ee73ea64d36e7b39ba765df2f153a5f58
mhrubel <bd.mhrubel@gmail.com>
2025-04-12 20:21:16 +0600

    Update

--- docs/docs-theme/tailwind.config.js
+++ docs/docs-theme/tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    content: [
-      '../public/**/*.html',
-      '../templates/**/*.twig'
-    ],
-    theme: {
-      extend: {
-        colors: {
-          'wp-blue': '#21759b',
-          'wp-gray': '#f1f1f1'
-        }
+  content: [
+    "../public/**/*.html",
+    "../templates/**/*.twig"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'wp-blue': '#21759b',
+        'wp-gray': '#f1f1f1'
       }
     }
-  }
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
+}
